18f60d63e6f6bb2de48efeb827a8da45428a1894
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-07-14 20:12:34 +0100

    Add inline option to markdown helper

--- addon/helpers/markdown.js
+++ addon/helpers/markdown.js
@@ -2,9 +2,10 @@ import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/string';
 import marked from 'marked';
 
-export function markdown([value='']) {
+export function markdown([value=''], { inline = false }) {
+
 	return htmlSafe(
-		marked( String(value) )
+		inline ? marked.inlineLexer(String(value), []) : marked(String(value))
 	);
 }
 
