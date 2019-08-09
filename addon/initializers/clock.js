f5fb420609f5b67d4410f10bef708db29cbe7b6d
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-08-09 10:58:34 +0100

    Don’t automatically inject clock service into app

--- addon/initializers/clock.js
+++
@@ -1,7 +0,0 @@
-export default function(application) {
-
-	application.inject('route', 'clock', 'service:clock');
-	application.inject('controller', 'clock', 'service:clock');
-	application.inject('component', 'clock', 'service:clock');
-
-}
