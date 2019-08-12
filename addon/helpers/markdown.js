33f0f51d108064acbd630db6a2638e4d13441e53
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-08-12 16:42:13 +0100

    Improve markdown helper

--- addon/helpers/markdown.js
+++ addon/helpers/markdown.js
@@ -2,22 +2,16 @@ import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/string';
 import marked from 'marked';
 
-const renderer = function() {
-	const renderer = new marked.Renderer();
-	renderer.paragraph = (text) => text + '<br><br>';
-	return renderer;
-};
-
 export function markdown([value=''], { inline = false }) {
 
 	switch (inline) {
 	case true:
-		return htmlSafe(marked(String(value), {
-			renderer: renderer(), gfm: true, breaks: true,
+		return htmlSafe(marked.inlineLexer(String(value), {
+			gfm: true, breaks: false,
 		}));
 	case false:
 		return htmlSafe(marked(String(value), {
-			gfm: true, breaks: true,
+			gfm: true, breaks: false,
 		}));
 	}
 
