633dbe2fa236c8cb54749fb4f53db4643f4829e3
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-08-15 03:07:10 +0100

    Ensure page renderer is cancelled before re-rendering

--- addon/components/app-document/viewer.js
+++ addon/components/app-document/viewer.js
@@ -24,37 +24,37 @@ export default Component.extend({
 
 	async process(doc, num) {
 
-		if (this.doc === undefined) return;
+		if (doc === undefined) return;
 
-		await timeout(100);
-
-		let page, ren;
+		await timeout(500);
 
 		try {
 
-			page = await doc.getPage(num);
+			if (this.ren) this.ren.cancel();
 
-			let vp = page.getViewport({ scale: 1 });
+			this.pge = await doc.getPage(num);
+
+			let vp = this.pge.getViewport({ scale: 1 });
 			let ww = this.element.offsetWidth - this.spacing;
 			let wh = this.element.offsetHeight - this.spacing;
 			let scale = Math.min(ww/vp.width, wh/vp.height);
 
-			let viewport = page.getViewport({ scale });
+			let viewport = this.pge.getViewport({ scale });
 			let canvas = this.element.querySelectorAll('canvas')[0];
 			let context = canvas.getContext('2d');
 
 			canvas.width = viewport.width;
 			canvas.height = viewport.height;
 
-			ren = await page.render({
+			this.ren = await this.pge.render({
 				canvasContext: context,
 				viewport: viewport,
 			});
 
 		} finally {
 
-			if (page && page.cleanup) page.cleanup();
-			if (ren && ren.cancel) ren.cancel();
+			if (this.pge && this.pge.cleanup) this.pge.cleanup();
+			if (this.ren && this.ren.cancel) this.ren.cancel();
 
 		}
 
