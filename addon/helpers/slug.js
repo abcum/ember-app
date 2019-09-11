91fc3caf3213d32d7774c59313883d7f1805ccef
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-09-11 13:18:40 +0100

    Update slugify function

--- addon/helpers/slug.js
+++ addon/helpers/slug.js
@@ -1,7 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function slug([value='']) {
-	return String(value).replace(/['"]/gi, '').replace(/[\s.]/gi, '-').replace(/(?![a-zA-Z0-9])/gi, '').toLowerCase();
+	return String(value).replace(/[\s.]/gi, '-').replace(/[^a-zA-Z0-9_-]+/g, '').replace(/--/, '-').toLowerCase();
 }
 
 export default helper(slug);
